8ef56ef48561d408c8f6724b41f4c037d9c021f8
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-09-21 20:40:14 +0530

    rendering error msg by using if-else conditional statement

--- map/learningMap/src/App.jsx
+++ map/learningMap/src/App.jsx
@@ -1,22 +1,31 @@
+import ErrorMsg from "./errormsg";
 
 function App() {
   let healthyItems = [
-    "banana",
-    "guava",
-    "apple",
-    "mango",
-    "papaya",
-    "pineapple",
-    "orange",
-    "strawberry",
+    // "banana",
+    // "guava",
+    // "apple",
+    // "mango",
+    // "papaya",
+    // "pineapple",
+    // "orange",
+    // "strawberry",
+    // "blueberry",
   ];
+  // if (healthyItems.length === 0) {
+  //   return <h3>I m Hungry</h3>
+  // }
+  let Msg = <h3>I m Hungry</h3>
+  if (healthyItems.length === 0) {
+    
+  }
   return (
     <>
       <h1>Keep Healthy</h1>
-      <ul class="list-group">
+      <ul className="list-group">
+        {Msg}
         {healthyItems.map((item) => {
-          return(
-          <li class="list-group-item">{item}</li>)
+          return <li key={item}className="list-group-item">{item}</li>;
         })}
       </ul>
     </>
--- map/learningMap/src/errormsg.jsx
+++ map/learningMap/src/errormsg.jsx
@@ -1,1 +1,16 @@
+function ErrorMsg() {
+  let healthyItems = [
+    "banana",
+    "guava",
+    "apple",
+    "mango",
+    "papaya",
+    "pineapple",
+    "orange",
+  ];
 
+  if (healthyItems.length === 0) {
+    return <h2>i m too hungry</h2>;
+  }
+}
+export default ErrorMsg;
